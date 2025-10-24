c60c3bec35422e9a0ecb789cc8772b6328f58a55
Aman Gautam <amangautam2128@gmail.com>
2025-10-24 20:40:50 +0530

    Configured S3

---
+++ src/main/java/com/cfs/backend/config/S3Config.java
@@ -0,0 +1,37 @@
+package com.cfs.backend.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Value("${minio.url:http://127.0.0.1:9000}")
+    private String minioUrl;
+
+    @Value("${minio.access.key:minioadmin}")
+    private String accessKey;
+
+    @Value("${minio.secret.key:minioadmin}")
+    private String secretKey;
+
+    public S3Client getS3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(minioUrl))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey,secretKey)
+                ))
+                .region(Region.US_EAST_1)
+                .forcePathStyle(true)
+                .build();
+    }
+
+
+}
